4438482700351762eb9f67defd9a3629b48c2609
himansh-23 <himanshu00096@gmail.com>
2019-06-27 23:44:05 +0530

    router guard added for dashboard

--- src/app/Services/RouteGurard/routeguard.service.ts
+++ src/app/Services/RouteGurard/routeguard.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { Router, CanActivate ,ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
-export class RouteguardService {
+export class RouteguardService  implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private router: Router,){}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):boolean
+  {
+    if(!localStorage.getItem('Token')){
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './Components/login/login.component';
 import { ForgotComponent } from './Components/forgot/forgot.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { ResetpageComponent } from './Components/resetpage/resetpage.component';
-
+import {RouteguardService} from './Services/RouteGurard/routeguard.service'
 const routes: Routes = [{path: 'register',component:RegisterComponent},
                         {path:'login',component:LoginComponent},
                         {path:'forgot',component:ForgotComponent},
-                        {path:'dashboard',component:DashboardComponent},
+                        {path:'dashboard',component:DashboardComponent, canActivate: [RouteguardService]},
                         {path:'resetpage',component:ResetpageComponent},
-                        {path:'', redirectTo:'login',pathMatch:"full"}];
+                        {path:'', redirectTo:'login',pathMatch:"full"},
+                        {path:'**' ,redirectTo:'login',pathMatch:"full"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
